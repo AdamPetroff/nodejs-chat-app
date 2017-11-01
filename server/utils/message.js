bda48b0bc76aaf3669ddd4cb1c0d171c33aa1cea
Adam Petroff <adam.petroff@cleevio.cz>
2017-11-01 14:19:57 +0100

    initial

---
+++ server/utils/message.js
@@ -0,0 +1,19 @@
+var moment = require('moment');
+
+var generateMessage = (from, text) => {
+    return {
+        from: from,
+        text: text,
+        createdAt: moment().valueOf()
+    };
+};
+
+var generateLocationMessage = (from, latitude, longitude) => {
+    return {
+        from,
+        url: `http://google.com/maps?q=${latitude},${longitude}`,
+        createdAt: moment().valueOf()
+    };
+};
+
+module.exports = {generateMessage, generateLocationMessage};
